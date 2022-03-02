6f128048a8f33ecb96dd090c53a22ee3044057f4
Raj <raajgupta011@gmail.com>
2022-03-02 21:48:33 +0530

    Min And Max

---
+++ MinAndMaxElementArray.java
@@ -0,0 +1,43 @@
+public class MinAndMaxElementArray {
+    public static void main(String[] args) {
+//        int[] arr = {1, 88, 89, 7, 8, 190};
+//        int arrSize = 6;
+        System.out.println(1112);
+//        Pair minMax =getMinMax(arr,arrSize);
+//        System.out.println("Maximum element in an array:- "+minMax.max);
+//        System.out.println("Minimum element in an Array:-" +minMax.min);
+//    }
+//    static class Pair{
+//        int max;
+//        int min;
+//    }
+//    static Pair getMinMax(int[]arr,int n){
+//
+//         Pair minMax= new Pair();
+//        // Only one element
+//        if(n==1){
+//            minMax.min=arr[0];
+//            minMax.max=arr[0];
+//            return minMax;
+//        }
+//        //For two element
+//            if(arr[0]>arr[1]){
+//                minMax.min=arr[1];
+//                minMax.max=arr[0];
+//            }else{
+//                minMax.min=arr[0];
+//                minMax.max=arr[1];
+//            }
+//            for(int i=2;i<n;i++) {
+//                if (arr[i] > minMax.max) {
+//                    minMax.max = arr[i];
+//                } else if (arr[i] < minMax.min) {
+//                    minMax.min = arr[i];
+//                }
+//            }
+//                return minMax;
+//            }
+//}
+//
+    }
+}
